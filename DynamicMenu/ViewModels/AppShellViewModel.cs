8efc19d227ce753f4fb569a43c97144a8a209426
Jan Seriš <jan.seris@seznam.cz>
2023-04-20 15:25:38 +0200

    added repo content forked from private repo

---
+++ DynamicMenu/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,84 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using DynamicMenu.EventArg;
+using DynamicMenu.Models;
+using DynamicMenu.Views;
+
+namespace DynamicMenu.ViewModels
+{
+    public partial class AppShellViewModel : ObservableObject
+    {
+        private readonly AppState appState;
+        public AppShellViewModel(AppState appState)
+        {
+            this.appState = appState;
+            LoginViewModel.SuccessfulLogin += OnSuccessfullLogin;
+        }
+
+        [ObservableProperty]
+        private string userName;
+
+        [ObservableProperty]
+        private bool archeologieMenuButtonVisible = false;
+
+        [ObservableProperty]
+        private bool bozpMenuButtonVisible = false;
+
+        [ObservableProperty]
+        private bool monitoringMenuButtonVisible = false;
+
+        private async void OnSuccessfullLogin(object sender, LoginEventArgs e)
+        {
+            var user = e.User;
+
+            appState.CurrentUser = user;
+            UserName = user.Name;
+
+            if (user.Rights.Count == 0)
+            {
+                await GoToLogin("Uživatel nemá žádná práva");
+                return;
+            }
+
+            SetMenuItemsByUserRights(user.Rights);
+            await GoToPageByUserRight(user.Rights.First());
+        }
+
+        [RelayCommand]
+        async Task Logout()
+        {
+            appState.CurrentUser = null;
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+
+        private async Task GoToLogin(string message)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+            await Shell.Current.DisplayAlert("Chyba", message, "OK");
+        }
+
+        private void SetMenuItemsByUserRights(List<MenuButtonRight> prava)
+        {
+            ArcheologieMenuButtonVisible = prava.Contains(MenuButtonRight.Archeologie);
+            BozpMenuButtonVisible = prava.Contains(MenuButtonRight.BOZP);
+            MonitoringMenuButtonVisible = prava.Contains(MenuButtonRight.Monitoring);
+        }
+
+        private async Task GoToPageByUserRight(MenuButtonRight pravo)
+        {
+            switch (pravo)
+            {
+                case MenuButtonRight.Archeologie:
+                    await Shell.Current.GoToAsync($"//{nameof(ArcheologiePage)}");
+                    break;
+                case MenuButtonRight.BOZP:
+                    await Shell.Current.GoToAsync($"//{nameof(BozpPage)}");
+                    break;
+                case MenuButtonRight.Monitoring:
+                    await Shell.Current.GoToAsync($"//{nameof(MonitoringPage)}");
+                    break;
+            }
+        }
+    }
+}
---
+++ DynamicMenu/ViewModels/LoginViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using DynamicMenu.EventArg;
+using DynamicMenu.Models;
+using DynamicMenu.Services;
+
+namespace DynamicMenu.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        public static event EventHandler<LoginEventArgs> SuccessfulLogin;
+        private readonly UserService service;
+        public LoginViewModel(UserService service)
+        {
+            this.service = service;
+        }
+
+        //default value can be set directly to observable property field here and it will work correctly
+
+        //not used actually atm
+        [ObservableProperty]
+        private bool reloadingUsers;
+
+        [ObservableProperty]
+        private ObservableCollection<User> availableUsers;
+
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private bool unknownUsernameTextVisible;
+
+        //warning: setting IsRefreshing property (bound to RefreshView) to true
+        //will enable the loading animation
+        //but also will execute the command set in RefreshView (ReloadUsers method in this case)
+        //https://github.com/dotnet/maui/issues/6456#issuecomment-1500983408
+        [ObservableProperty]
+        private bool isRefreshing;
+
+        private void ResetForm()
+        {
+            AvailableUsers = new ObservableCollection<User>(new List<User>());
+            Name = string.Empty;
+            UnknownUsernameTextVisible = false;
+        }
+
+        [RelayCommand]
+        async void OnAppearing()
+        {
+            ResetForm();
+            await ReloadUsers();
+        }
+
+        [RelayCommand]
+        void OnNameChanged()
+        {
+            UnknownUsernameTextVisible = false;
+        }
+
+        [RelayCommand]
+        async Task Login()
+        {
+            var inputText = Name;
+            var user = await service.GetUserByName(inputText);
+            if (user is null)
+            {
+                UnknownUsernameTextVisible = true;
+                return;
+            }
+
+            SuccessfulLogin?.Invoke(this, new LoginEventArgs(user));
+        }
+
+        //prevent duplicate calls when IsRefreshing is set to true to be able to show loading animation
+        //https://github.com/dotnet/maui/issues/6456#issuecomment-1500983408
+        private bool alreadyReloadingUsers;
+
+        [RelayCommand]
+        async Task ReloadUsers()
+        {
+            if (alreadyReloadingUsers)
+            {
+                return;
+            }
+            alreadyReloadingUsers = true;
+
+            //RefreshView loading animation
+            if (IsRefreshing != true)
+            {
+                //enables loading animation on RefreshView
+                //but also triggers this function by coincidence via registered Command on RefreshView
+                IsRefreshing = true; 
+            }
+
+            ReloadingUsers = true;
+            var items = await service.GetAll();
+            AvailableUsers = new ObservableCollection<User>(items);
+            ReloadingUsers = false;
+
+            IsRefreshing = false; //RefreshView loading animation
+
+            alreadyReloadingUsers = false;
+        }
+    }
+}
